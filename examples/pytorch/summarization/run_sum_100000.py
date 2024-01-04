de6a28fbbec545bd1920e9df3baca09aec6845d7
charon52hz <charon52hz@gmail.com>
2024-01-04 21:54:49 +0800

    1/4sentence loss

--- examples/pytorch/summarization/run_sum_100000.py
+++ examples/pytorch/summarization/run_sum_100000.py
@@ -23,7 +23,7 @@ from transformers import (
 # torch.cuda.manual_seed_all(seed)
 
 # 加载数据集
-from transformers.models.bart.modeling_bart_seqLoss_highDim import BartForConditionalGeneration
+from transformers.models.bart.modeling_bart_seqLoss3 import BartForConditionalGeneration
 
 train_dataset = load_dataset('json', data_files='./data/train_subsets.json')
 test_dataset = load_dataset('json', data_files='./data/test_public.json')
@@ -55,7 +55,7 @@ model.resize_token_embeddings(len(tokenizer))
 
 batch_size = 128
 args = Seq2SeqTrainingArguments(
-    output_dir="output/lcstsm/sentenceLoss_highDim2",
+    output_dir="output/lcstsm/sentenceLoss3",
     num_train_epochs=20,
     do_train=True,
     do_eval=True,
